03f4768350641a8c3a11e019f086d96b7bfc02ba
oscarmoore22 <oscarmoore2@icloud.com>
2026-05-25 18:23:04 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'stalbridge-v1';
+const ASSETS = ['/stalbridge-fantasy/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
